db88cf9f7db51f6722d4a938b6cf5172d6677900
soomin.lee <soomni02@gmail.com>
2022-01-25 08:52:35 +0900

    memory api commit

--- src/main/java/com/example/demo/src/memory/MemoryDao.java
+++ src/main/java/com/example/demo/src/memory/MemoryDao.java
@@ -1,5 +1,6 @@
 package com.example.demo.src.memory;
 
+import com.example.demo.src.memory.model.GetAnsweredMemoryRes;
 import com.example.demo.src.memory.model.GetNotAnsweredMemoryRes;
 import com.example.demo.src.memory.model.GetOneMemoryRes;
 import com.example.demo.src.ready.model.GetReadyRes;
@@ -25,15 +26,22 @@ public class MemoryDao {
      * @return BaseResponse<List<GetNotAnsweredMemoryRes>>
      * */
     public List<GetNotAnsweredMemoryRes> getNotAnsweredMemory(int userIdx, int petIdx){
-        String getNotAnsweredMemoryQuery = "";
+        String Query = "SELECT mq.idx AS questionNum, " +
+                "REPLACE(mq.context, '*', " +
+                "(SELECT p.name FROM pet p WHERE p.idx = ?)) as question," +
+                " ma.context AS answer\n" +
+                "FROM memory_question mq\n" +
+                "         LEFT JOIN memory_answer ma on mq.idx = ma.mqIdx\n" +
+                "WHERE ISNULL(ma.context)\n" +
+                ";";
 
         int Params = petIdx;
 
-        return this.jdbcTemplate.query(getNotAnsweredMemoryQuery,
+        return this.jdbcTemplate.query(Query,
                 (rs, rowNum) -> new GetNotAnsweredMemoryRes(
                         rs.getInt("questionNum"),
                         rs.getString("question"),
-                        rs.getString("existenceOfAnswer")
+                        rs.getString("answer")
                 ),Params);
     }
 
@@ -44,7 +52,26 @@ public class MemoryDao {
      * [GET] /ready/:userIdx/:petIdx
      * @return BaseResponse<List<GetAnsweredMemoryRes>>
      * */
+    public List<GetAnsweredMemoryRes> getAnsweredMemory(int userIdx, int petIdx){
+        String Query = "SELECT mq.idx AS questionNum,\n" +
+                "       REPLACE(mq.context, '*', (SELECT p.name FROM pet p WHERE p.idx = ?)) as question,\n" +
+                "       ma.context AS answer, date_format(ma.updatedAt, '%Y년 %m월 %d일') as updatedAt\n" +
+                "FROM memory_question mq\n" +
+                "         LEFT JOIN memory_answer ma on mq.idx = ma.mqIdx\n" +
+                "WHERE ma.context IS NOT NULL\n" +
+                "ORDER BY questionNum\n" +
+                ";";
 
+        int Params = petIdx;
+
+        return this.jdbcTemplate.query(Query,
+                (rs, rowNum) -> new GetAnsweredMemoryRes(
+                        rs.getInt("questionNum"),
+                        rs.getString("question"),
+                        rs.getString("answer"),
+                        rs.getString("updatedAt")
+                ),Params);
+    }
 
 
 
@@ -53,4 +80,23 @@ public class MemoryDao {
      * [GET] /ready/:userIdx/:readyAnswerIdx
      * @return BaseResponse<List<GetOneMemoryRes>>
      * */
+    public List<GetOneMemoryRes> getOneMemory(int userIdx, int petIdx){
+        String Query = "SELECT mq.idx as questionNum, REPLACE(mq.context, '*', p.name) as question, " +
+                "ma.context as answer\n" +
+                "    ,date_format(ma.updatedAt, '%Y년 %m월 %d일') as updatedAt\n" +
+                "FROM memory_question mq\n" +
+                "         LEFT JOIN memory_answer ma on mq.idx = ma.mqIdx\n" +
+                "         LEFT JOIN pet p on ma.petIdx = p.idx\n" +
+                "WHERE ma.idx = 2;";
+
+        int Params = petIdx;
+
+        return this.jdbcTemplate.query(Query,
+                (rs, rowNum) -> new GetOneMemoryRes(
+                        rs.getInt("questionNum"),
+                        rs.getString("question"),
+                        rs.getString("answer"),
+                        rs.getString("updatedAt")
+                ),Params);
+    }
 }
--- src/main/java/com/example/demo/src/memory/model/GetAnsweredMemoryRes.java
+++ src/main/java/com/example/demo/src/memory/model/GetAnsweredMemoryRes.java
@@ -1,4 +1,15 @@
 package com.example.demo.src.memory.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class GetAnsweredMemoryRes {
+    private int questionNum;
+    private String question;
+    private String answer;
+    private String updatedAt;
 }
